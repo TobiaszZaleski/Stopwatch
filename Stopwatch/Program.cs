8b2d29d7298f6d4d2e67e17ccadad01218d6a537
TobiaszZaleski <tobiasz.zaleski@gmail.com>
2021-09-27 13:23:54 +0200

    Add project files.

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            bool Condition = true;
+            var Pomiar = new Stopwatch();
+
+
+            while (true)
+            {
+                while (true)
+                {
+                    Console.WriteLine("Wpisz \"Start\", aby zacząć odliczanie oraz \"Stop\" aby je zatrzymac, mozesz wpisac \"Zakoncz\" \naby zakończyć działanie programu");
+                    var tekst = Console.ReadLine();
+
+
+                    if (tekst == "Start")
+                    {
+                        Pomiar.Start();
+                        break;
+                    }
+                    else if (tekst == "Zakoncz")
+                    {
+                        Condition = false;
+                        break;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Taka operacja nie może zostać wykonana");
+                        continue;
+                    }
+                }
+
+
+
+
+                if (Condition==false)
+                {
+                    break;
+                }
+
+
+
+
+                while (true)
+                {
+
+                    Console.WriteLine("Aby zakonczyc pomiar wpisz \"Stop\", aby wykonac miedzyczas wpisz \"Nastepny\" lub \"Zakoncz\" aby wyłączyć program.");
+                    var tekst2 = Console.ReadLine();
+
+
+                    if (tekst2 == "Nastepny")
+                    {
+                        Console.WriteLine("Stop Message: " + Pomiar.Stop);
+                    }
+                    else if (tekst2 == "Stop")
+                    {
+                        Console.WriteLine("Stop Message: " + Pomiar.Stop);
+                        break;
+                    }
+                    else if (tekst2 == "Zakoncz")
+                    {
+                        Condition = false;
+                        break;
+                    }
+                    else
+                        throw new InvalidOperationException("Taka operacja nie może zostać wykonana po rozpoczęciu odliczania");
+                }
+
+
+                if (Condition == false)
+                {
+                    break;
+                }
+
+
+                Console.WriteLine("Wpisz \"1\" aby kontynuować lub \"2\" aby zakonczyc");
+                var tekst3 = Console.ReadLine();
+
+
+                if (tekst3 == "2")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    continue;
+                }
+            }
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        public DateTime StartTime { get; set; }
+        public TimeSpan NextTime { get; set; }
+
+        public Stopwatch()
+        {
+            StartTime = DateTime.Now;
+        }
+        public void Start()
+        {
+            NextTime = DateTime.Now - StartTime;
+        }
+        public double Stop
+        {
+            get 
+            {
+                var timeSpan = DateTime.Now - StartTime - NextTime;
+                return timeSpan.TotalSeconds;
+            }
+        }
+    }
+}
